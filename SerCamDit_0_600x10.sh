7a4738fd175cb6fef23c6caa98e51a66e825445c
arided02 <41963241+arided02@users.noreply.github.com>
2022-02-26 21:34:43 +0800

    Update SerCamDit_0_600x10.sh
    Update the shell script to prevent sscv2 catch the same /dev/ttyUSB0 device. This bug will be let USBto232 cable non-acked post exposure and looks another sscv2 threads's signal.

--- SerCamDit_0_600x10.sh
+++ SerCamDit_0_600x10.sh
@@ -1,21 +1,44 @@
 #!/bin/bash
 a="indi_"
 b="phd2.bin"
-
+c="/dev/ttyUSB1" #serial port # by USBto232 
+d="18" ## for dithering wait seconds need user tune to fit user's equatorial mounts dithering to stable time.
 pgrep ${a}  &> /dev/null && uuuid0=0 || uuuid0=1
 pgrep ${b}  &> /dev/null && uuuid1=0 || uuuid1=1
+ps ax|grep ${c}|grep -v grep  &> /dev/null && uuuid2=0 || uuuid2=1 
+echo "check ${a}/${b}/${c}:" $uuuid0 / $uuuid1 / $uuuid2 "0 means pre-executed"
+## if no ind_* nor phd2 then exit
 
-echo $uuuid0 / $uuuid1
+#check indi_* drivers and phd2 executed
 if [ "${uuuid0}" == 1 ] || [ "${uuuid1}" == 1 ]; then
-    echo "Both ${a} and ${b} need to be executed.."
+    echo "Both ${a}** drivers and ${b} need to be executed..please preload indi_ drivers in INDISTARTER or manual execute it"
+    sleep 6
     exit 1
 fi
-echo "ready to shoot stars, have a good night."
+#check /ttyUSB0 occupied by other sscv2
+if [ "${uuuid2}" == 0 ]; then
+    echo "Serial port ${c} was occupied, please try another /dev/ttyUSBn..."
+    sleep 3
+    exit 1
+#else  ##recheck again if in phd2 dithering period
+#    echo "check if phd in dithering phase"
+#    sleep ${d}
+#    ps ax|grep ${c}|grep -v grep  &> /dev/null && uuuid3=0 || uuuid3=1
+#    if [ "${uuuid3}" == 0 ]; then
+#        echo "Serial port ${c} was occupied on post dithering. Try another /dev/ttyUSBnnn serial port"
+#        sleep 3
+#        exit 1
+#    fi
+#    echo "nor occupied or phd2 dithering"
+fi
+
+
+echo "${c} was not occupied, ready to use sscv2 to shoot stars, have a good night."
 
 now=$(date +"%Y%m%d_%H%M%S")
 
 echo $now ":start exposure with dither"
-/usr/bin/sscv2 -s /dev/ttyUSB0 -c 10 -t 600 -p 10 -d 3    ## >> ~/PHD2/ssc_$now.txt
+/usr/bin/sscv2 -s ${c} -c 30 -t 478 -p ${d} -d 2 -m 2  ## >> ~/PHD2/ssc_$now.txt
 now=$(date +"%Y/%m/%d_%H:%M:%S")
 echo $now ":finished exposure."
 #echo "Press enter....."
