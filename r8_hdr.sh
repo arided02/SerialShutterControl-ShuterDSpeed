2af59bbfcab06d8d3aac72849763d4a0d5dbf0b9
Arided T.L. Shen <41963241+arided02@users.noreply.github.com>
2024-01-18 11:48:09 +0800

    Update r8_hdr.sh
    debug

--- r8_hdr.sh
+++ r8_hdr.sh
@@ -4,28 +4,53 @@ gphoto2 --set-config capturetarget=1
 gphoto2 --set-config iso=100
 SECONDS=0
 a=$SECONDS
-for i  in {55..15};
+timethread=30 ##30 for r8 seconds per 5 div
+five=0 ## mod 5 =0 interval 5
+rej=0
+startno=55
+endno=1
+for i  in {55..14};
 do
-        b=$SECONDS
-        echo $i;
-        gphoto2 --set-config shutterspeed=$i
-        gphoto2 --capture-image
-        sleep 0.4
-        duration=$SECONDS
-        echo "$(( duration - b )) seconds"
+	b=$SECONDS
+	echo $i;
+	if [ $i -le 30 ]; then
+	        
+		if [ $(( $i % 5 )) -ne 0 ]; then
+                   gphoto2 --set-config shutterspeed=$i
+                   gphoto2 --capture-image
+                else
+                   $rej = $rej+1   
+		fi   
+	else
+	    gphoto2 --set-config shutterspeed=$i
+	    gphoto2 --capture-image
+	fi   
+        sleep 0.05
+	duration=$SECONDS
+	echo "$(( duration - b )) seconds"
 done
-for j in {15..55};
+for j in {14..55};
 do
 
-        c=$SECONDS
-        echo $j;
-        gphoto2 --set-config shutterspeed=$j
-        gphoto2 --capture-image
-        sleep 0.3
-        duration=$SECONDS
-        echo "$(( duration - c )) seconds"
+	c=$SECONDS
+	if [  $j -le $timethread ]; then
+	        
+		if [ $(( $j % 5 )) -ne 0 ]; then
+	           echo $j;
+ 	           gphoto2 --set-config shutterspeed=$j
+	           gphoto2 --capture-image
+	        else
+	           $rej = $rej+1   
+		fi
+	else
+	   gphoto2 --set-config shutterspeed=$j
+           gphoto2 --capture-image
+        fi	   
+	sleep 0.05
+	duration=$SECONDS
+	echo "$(( duration - c )) seconds"
 done
 duration=$SECONDS
-echo "average EOS R8 $(((duration-a)/(55-15+1)/2)) fpm"
+echo "average EOS R8 $(((duration-a)/((55-14+1)*2+$rej))) fpm"
 
 
